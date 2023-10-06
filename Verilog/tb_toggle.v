8de85129a492b6713541ea0811675174d7ee797c
Sinaei <sina@hyperdbg.org>
2023-10-06 17:22:29 +0900

    add testbench for verilog vhdl

--- Verilog/tb_toggle.v
+++ Verilog/tb_toggle.v
@@ -2,42 +2,28 @@ module Switch_Control_tb;
 
   // Inputs
   reg clock;
-  
+
   // Outputs
-  wire switch;
+  reg switch;
 
-  // Instantiate the Switch_Control module
-  Switch_Control DUT (
-    .clock(clock),
-    .switch(switch)
-  );
-
-  // Define clock parameters
-  reg clock_enable = 1;
-  reg#1 clock_period = 10; // Clock period in time units (adjust as needed)
-  
-  // Testbench clock generation
-  always #clock_period clock = clock_enable ? ~clock : clock;
+  // Clock generation process
+  always begin
+    #5 clock = ~clock; // Toggle the clock every 5 time units (adjust as needed)
+  end
 
   // Initialize signals
   initial begin
     clock = 0;
-    clock_enable = 1;
-    
-    // Initialize simulation time
-    $dumpfile("Switch_Control_tb.vcd");
-    $dumpvars(0, Switch_Control_tb);
-    
+    switch = 0;
+
     // Simulate for some time
     #100;
-    
+
+    // Monitor the switch signal
+    $display("Switch = %b", switch);
+
     // Stop simulation
     $finish;
-  end
-
-  // Monitor the switch signal
-  always @(posedge clock) begin
-    $display("Switch = %b", switch);
   end
 
 endmodule
